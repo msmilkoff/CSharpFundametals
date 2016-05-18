d2d9f0260c265b12f989c44277fe4439bb208c73
msmilkoff <msmilkoff@gmail.com>
2016-05-18 15:59:17 +0300

    Added recursive Fibonacci solution

--- Advanced C# - May-2016/AdvancedCSharpExercises/StacksAndQueues/06.TruckTour/TruckTour.cs
+++ Advanced C# - May-2016/AdvancedCSharpExercises/StacksAndQueues/06.TruckTour/TruckTour.cs
@@ -12,13 +12,7 @@
             var truck  = new Queue<int>();
             for (int i = 0; i < pumpsCount; i++)
             {
-                string[] inputArgs = Console.ReadLine().Trim().Split();
-
-                var petrolAmount = int.Parse(inputArgs[0]);
-                var distance = int.Parse(inputArgs[1]);
-
-                truck.Enqueue(petrolAmount);
-                // TODO: the rest
+                // TODO
             }
         }
     }
---
+++ Advanced C# - May-2016/AdvancedCSharpExercises/StacksAndQueues/08.RecursiveFibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.RecursiveFibonacci
+{
+    using System;
+
+    public class RecursiveFibonacci
+    {
+        public static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(n));
+        }
+
+        private static long GetFibonacci(long n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            return GetFibonacci(n - 2) + GetFibonacci(n - 1);
+        }
+    }
+}
