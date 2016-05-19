43320deff9e98e55f9eecf805ba91fd74e05188e
msmilkoff <msmilkoff@gmail.com>
2016-05-19 11:56:05 +0300

    Added base functionality for 'BashSoft'

---
+++ Advanced C# - May-2016/Labs/StoryMode/BashSoft/IOManager.cs
@@ -0,0 +1,31 @@
+﻿namespace BashSoft
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public static class IOManager
+    {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentation = path.Split('\\').Length;
+
+            var subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+
+            while (subFolders.Count != 0)
+            {
+                string currentPath = subFolders.Dequeue();
+                int identation = currentPath.Split('\\').Length - initialIdentation;
+                var ident = new string('-', identation);
+                OutputWriter.WriteMessageOnNewLine($"{ident}{currentPath}");
+
+                var directories = Directory.GetDirectories(currentPath);
+                foreach (var directoryPath in directories)
+                {
+                    subFolders.Enqueue(directoryPath);
+                }
+            }
+        }
+    }
+}
---
+++ Advanced C# - May-2016/Labs/StoryMode/BashSoft/Launcher.cs
@@ -0,0 +1,11 @@
+﻿namespace BashSoft
+{
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\SoftUni\C# Fundamentals\Advanced C# - May-2016\Labs\StoryMode";
+            IOManager.TraverseDirectory(path);
+        }
+    }
+}
