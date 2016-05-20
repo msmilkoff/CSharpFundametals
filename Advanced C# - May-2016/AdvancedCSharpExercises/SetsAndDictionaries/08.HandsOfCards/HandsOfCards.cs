6304cfbc59d2502a7d9886c1a67ab209e6805575
msmilkoff <msmilkoff@gmail.com>
2016-05-20 20:49:16 +0300

    Added solutions for HandsOfCards and UserLogs

--- Advanced C# - May-2016/AdvancedCSharpExercises/SetsAndDictionaries/08.HandsOfCards/HandsOfCards.cs
+++ Advanced C# - May-2016/AdvancedCSharpExercises/SetsAndDictionaries/08.HandsOfCards/HandsOfCards.cs
@@ -2,12 +2,112 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class HandsOfCards
     {
         public static void Main(string[] args)
         {
-            //TODO
+            // TODO: Optimize for maximum result in Judge.
+            var cardsByPlayer = new Dictionary<string, HashSet<string>>();
+
+            string input = Console.ReadLine();
+            while (input != "JOKER")
+            {
+                string[] inputArgs = input.Trim().Split(
+                    new[] { ':', ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string playerName = inputArgs[0];
+                var handOfCards = new HashSet<string>();
+                for (int i = 1; i < inputArgs.Length; i++)
+                {
+                    handOfCards.Add(inputArgs[i]);
+                }
+
+                if (!cardsByPlayer.ContainsKey(playerName))
+                {
+                    cardsByPlayer.Add(playerName, handOfCards);
+                }
+                else
+                {
+                    AddHandToPlayer(handOfCards, cardsByPlayer, playerName);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var playerHandPair in cardsByPlayer)
+            {
+                int points = CalculateHand(playerHandPair);
+                Console.WriteLine($"{playerHandPair.Key}: {points}");
+            }
+        }
+
+        private static int CalculateHand(KeyValuePair<string, HashSet<string>> playerHandPair)
+        {
+            int points = 0;
+
+            foreach (var card in playerHandPair.Value)
+            {
+                int power, multiplier = 0;
+
+                if (card.Length == 3)
+                {
+                    power = 10;
+                }
+                else
+                {
+                    switch (card[0])
+                    {
+                        case 'J':
+                            power = 11;
+                            break;
+                        case 'Q':
+                            power = 12;
+                            break;
+                        case 'K':
+                            power = 13;
+                            break;
+                        case 'A':
+                            power = 14;
+                            break;
+                        default:
+                            power = int.Parse(card[0].ToString());
+                            break;
+                    }
+                }
+
+                switch (card.Last())
+                {
+                    case 'S':
+                        multiplier = 4;
+                        break;
+                    case 'H':
+                        multiplier = 3;
+                        break;
+                    case 'D':
+                        multiplier = 2;
+                        break;
+                    case 'C':
+                        multiplier = 1;
+                        break;
+                }
+
+                points += power * multiplier;
+            }
+
+            return points;
+        }
+
+        private static void AddHandToPlayer(HashSet<string> handOfCards, Dictionary<string, HashSet<string>> cardsByPlayer, string playerName)
+        {
+            foreach (var hand in handOfCards)
+            {
+                if (!cardsByPlayer[playerName].Contains(hand))
+                {
+                    cardsByPlayer[playerName].Add(hand);
+                }
+            }
         }
     }
 }
---
+++ Advanced C# - May-2016/AdvancedCSharpExercises/SetsAndDictionaries/09.UserLogs/UserLogs.cs
@@ -0,0 +1,56 @@
+﻿namespace _09.UserLogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class UserLogs
+    {
+        public static void Main(string[] args)
+        {
+            var messagesByUser = new SortedDictionary<string, Dictionary<string, int>>();
+
+            const string pattern = @"IP=(.*?)message=.+? user=(\w+)";
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                var match = Regex.Match(command, pattern);
+                string user = match.Groups[2].Value;
+                string ipAdress = match.Groups[1].Value;
+
+                if (!messagesByUser.ContainsKey(user))
+                {
+                    messagesByUser.Add(user, new Dictionary<string, int>());
+                }
+
+                if (!messagesByUser[user].ContainsKey(ipAdress))
+                {
+                    messagesByUser[user].Add(ipAdress, 1);
+                }
+                else
+                {
+                    messagesByUser[user][ipAdress]++;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var outerPair in messagesByUser)
+            {
+                var output = new List<string>();
+
+                Console.WriteLine($"{outerPair.Key}:");
+                foreach (var innerPair in messagesByUser[outerPair.Key])
+                {
+                    string line = $"{innerPair.Key}=> {innerPair.Value}";
+                    output.Add(line);
+                }
+
+                Console.Write(string.Join(", ", output));
+                Console.WriteLine(".");
+            }
+        }
+    }
+}
