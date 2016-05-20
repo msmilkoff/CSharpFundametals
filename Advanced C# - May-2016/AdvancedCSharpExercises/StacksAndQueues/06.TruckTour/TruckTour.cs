64fcd9801ddffce0a80d0d08e4a3181b63a18d62
msmilkoff <msmilkoff@gmail.com>
2016-05-20 15:02:31 +0300

    Added TruckTour and StackFibonacci solutions

--- Advanced C# - May-2016/AdvancedCSharpExercises/StacksAndQueues/06.TruckTour/TruckTour.cs
+++ Advanced C# - May-2016/AdvancedCSharpExercises/StacksAndQueues/06.TruckTour/TruckTour.cs
@@ -9,10 +9,62 @@
         {
             int pumpsCount = int.Parse(Console.ReadLine());
 
-            var truck  = new Queue<int>();
+            var truck = new Queue<Pump>();
             for (int i = 0; i < pumpsCount; i++)
             {
-                // TODO
+                string[] pumpInfo = Console.ReadLine().Split();
+                int amount = int.Parse(pumpInfo[0]);
+                int distanceToNext = int.Parse(pumpInfo[1]);
+
+                var currentPump = new Pump(amount, distanceToNext, i);
+                truck.Enqueue(currentPump);
+            }
+
+            while (true)
+            {
+                var currentPump = truck.Dequeue();
+                truck.Enqueue(currentPump);
+
+                var startingPump = currentPump;
+                int gasInPump = currentPump.Amount;
+
+                bool canFinishTour = false;
+                while (gasInPump >= currentPump.DistanceToNext)
+                {
+                    gasInPump -= currentPump.DistanceToNext;
+
+                    currentPump = truck.Dequeue();
+                    truck.Enqueue(currentPump);
+                    if (currentPump == startingPump)
+                    {
+                        canFinishTour = true;
+                        break;
+                    }
+
+                    gasInPump += currentPump.Amount;
+                }
+
+                if (canFinishTour)
+                {
+                    Console.WriteLine(startingPump.PumpIndex);
+                    break;
+                }
+            }
+        }
+
+        public class Pump
+        {
+            public int Amount { get; set; }
+
+            public int DistanceToNext { get; set; }
+
+            public int PumpIndex { get; set; }
+
+            public Pump(int amount, int distanceToNext, int pumpIndex)
+            {
+                this.Amount = amount;
+                this.DistanceToNext = distanceToNext;
+                this.PumpIndex = pumpIndex;
             }
         }
     }
---
+++ Advanced C# - May-2016/AdvancedCSharpExercises/StacksAndQueues/09.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,29 @@
+﻿namespace _09.StackFibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StackFibonacci
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var fibNumbers = new Stack<ulong>();
+            fibNumbers.Push(1);
+            fibNumbers.Push(1);
+
+            for (int i = 1; i < n; i++)
+            {
+                ulong first = fibNumbers.Pop();
+                ulong second = fibNumbers.Pop();
+
+                fibNumbers.Push(first);
+                fibNumbers.Push(first + second);
+            }
+
+            fibNumbers.Pop();
+            Console.WriteLine(fibNumbers.Peek());
+        }
+    }
+}
