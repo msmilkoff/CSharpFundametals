366c6f8ef9db6c0bc723ec407ba30141b620d4ce
msmilkoff <msmilkoff@gmail.com>
2016-05-21 13:19:19 +0300

    Pushed solution for SimpleTextEditor

---
+++ Advanced C# - May-2016/AdvancedCSharpExercises/StacksAndQueues/10.SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,50 @@
+﻿namespace _10.SimpleTextEditor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            var operationResults = new Stack<StringBuilder>();
+
+            int operationsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < operationsCount; i++)
+            {
+                string[] commandArgs = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string commandType = commandArgs[0];
+                switch (commandType)
+                {
+                    case "1":
+                        var editedText = operationResults.Count > 0 
+                            ? new StringBuilder(operationResults.Peek().ToString()) 
+                            : new StringBuilder();
+                        editedText.Append(commandArgs[1]);
+                        operationResults.Push(editedText);
+                        break;
+                    case "2":
+                        var curretTextState = operationResults.Peek();
+                        var newTextState = new StringBuilder(curretTextState.ToString());
+                        int startIndex = curretTextState.Length - int.Parse(commandArgs[1]);
+                        newTextState.Remove(startIndex, curretTextState.Length - startIndex);
+                        operationResults.Push(newTextState);
+                        break;
+                    case "3":
+                        var currentTextState = operationResults.Peek();
+                        // wantedIndex is the N-th character of the text, so it's 1-based;
+                        int wantedIndex = int.Parse(commandArgs[1]) - 1;
+                        Console.WriteLine(currentTextState[wantedIndex]);
+                        break;
+                    case "4":
+                        operationResults.Pop();
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
