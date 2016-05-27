83ce1021a9fe0fe96407b800717670e86d9813cf
msmilkoff <msmilkoff@gmail.com>
2016-05-27 18:16:50 +0300

    created new project for the string processing homework

---
+++ Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/01.ReverseString/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.ReverseString
+{
+    using System;
+
+    public class ReverseString
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/02.StringLength/StringLength.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.StringLength
+{
+    using System;
+    using System.Linq;
+
+    public class StringLength
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length < 20)
+            {
+                Console.WriteLine(input + new string('*', 20 - input.Length));
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", input.Take(20)));
+            }
+        }
+    }
+}
