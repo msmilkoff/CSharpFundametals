a6ba2367f13e67827d7e722974597a53d1a77051
msmilkoff <msmilkoff@gmail.com>
2016-05-27 18:25:00 +0300

    manual string processing first commit

--- Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/03.FormattingNumbers/FormattingNumbers.cs
+++ Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/03.FormattingNumbers/FormattingNumbers.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _03.FormattingNumbers
+﻿namespace _03.FormattingNumbers
 {
-    class Program
+    using System;
+
+    public class FormattingNumbers
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
         }
     }
 }
