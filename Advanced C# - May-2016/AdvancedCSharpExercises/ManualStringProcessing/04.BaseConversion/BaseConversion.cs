202a6819e751c198c3a485188f3aabc02a1884e0
msmilkoff <msmilkoff@gmail.com>
2016-06-04 12:33:23 +0300

    optimized base-conversion problems

--- Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/04.BaseConversion/BaseConversion.cs
+++ Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/04.BaseConversion/BaseConversion.cs
@@ -1,12 +1,57 @@
 ﻿namespace _05.BaseNToBaseTen
 {
     using System;
+    using System.Numerics;
 
     public class BaseConversion
     {
         public static void Main(string[] args)
         {
-            // TODO:
+            var input = Console.ReadLine().Split();
+
+            int @base = int.Parse(input[0]);
+            string number = input[1];
+            if (@base == 10)
+            {
+                Console.WriteLine(number);
+                return;
+            }
+
+            Console.WriteLine(ConvertToDecimal(number, @base));
+        }
+
+        private static BigInteger ConvertToDecimal(string number, int @base)
+        {
+            var result = BigInteger.Zero;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int currentDigit = int.Parse(number[i].ToString());
+                int currentExponent = number.Length - i - 1;
+
+                result += currentDigit * PowerUp(@base, currentExponent);
+            }
+
+            return result;
+        }
+
+        private static BigInteger PowerUp(BigInteger number, int exponent)
+        {
+            if (exponent < 0)
+            {
+                return -1; // invalid exponent
+            }
+            else if (exponent == 1)
+            {
+                return number;
+            }
+            else if (exponent == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * PowerUp(number, exponent - 1);
+            }
         }
     }
 }
--- Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/04.BaseTenToAnyBase/BaseTenToAnyBase.cs
+++ Advanced C# - May-2016/AdvancedCSharpExercises/ManualStringProcessing/04.BaseTenToAnyBase/BaseTenToAnyBase.cs
@@ -1,9 +1,38 @@
 ﻿namespace _04.BaseTenToAnyBase
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
     public class BaseTenToAnyBase
     {
         public static void Main(string[] args)
         {
+            var input = Console.ReadLine().Split();
+
+            int @base = int.Parse(input[0]);
+            var number = BigInteger.Parse(input[1]);
+
+            string toNBase = ConvertToBaseN(number, @base);
+            Console.WriteLine(toNBase);
+        }
+
+        public static string ConvertToBaseN(BigInteger number, int @base)
+        {
+            var stack = new Stack<string>();
+            while (number >= @base)
+            {
+                stack.Push((number % @base).ToString());
+                number /= @base;
+            }
+
+            string result = "" + number;
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+
+            return result;
         }
     }
 }
