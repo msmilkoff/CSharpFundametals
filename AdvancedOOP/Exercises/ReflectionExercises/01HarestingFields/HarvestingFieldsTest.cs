bba284aac9c260bf815cda952d9f0dda65b6ca0f
msmilkoff <msmilkoff@gmail.com>
2016-07-25 20:57:06 +0300

    added reflection exercises

---
+++ AdvancedOOP/Exercises/ReflectionExercises/01HarestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,77 @@
+﻿namespace _01HarestingFields
+{
+    using System;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            var type = typeof(HarvestingFields);
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            string input = Console.ReadLine();
+            while (input != "HARVEST")
+            {
+                switch (input)
+                {
+                    case "private":
+                        foreach (var field in fields)
+                        {
+                            if (field.IsPrivate)
+                            {
+                                Console.WriteLine($"private {field.FieldType.Name} {field.Name}");
+                            }
+                        }
+                        break;
+                    case "public":
+                        foreach (var field in fields)
+                        {
+                            if (field.IsPublic)
+                            {
+                                Console.WriteLine($"public {field.FieldType.Name} {field.Name}");
+                            }
+                        }
+                        break;
+                    case "protected":
+                        foreach (var field in fields)
+                        {
+                            if (field.IsFamily)
+                            {
+                                Console.WriteLine($"protected {field.FieldType.Name} {field.Name}");
+                            }
+                        }
+                        break;
+                    case "all":
+                        foreach (var field in fields)
+                        {
+                            Console.WriteLine($"{GetFieldModifier(field)} {field.FieldType.Name} {field.Name}");
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        public static string GetFieldModifier(FieldInfo field)
+        {
+            if (field.IsPrivate)
+            {
+                return "private";
+            }
+            else if (field.IsPublic)
+            {
+                return "public";
+            }
+            else if (field.IsAssembly)
+            {
+                return "internal";
+            }
+            else
+            {
+                return "protected";
+            }
+        }
+    }
+}
---
+++ AdvancedOOP/Exercises/ReflectionExercises/02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,34 @@
+﻿namespace _02BlackBoxInteger
+{
+    using System;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main(string[] args)
+        {
+            var blackBox = typeof(BlackBoxInt);
+            var boxInstance = (BlackBoxInt)Activator.CreateInstance(
+                typeof(BlackBoxInt), true);
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                var tokens = input.Split('_');
+                string methodName = tokens[0];
+                int methodParam = int.Parse(tokens[1]);
+
+                var method = blackBox.GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
+                method.Invoke(boxInstance, new object[] { methodParam });
+
+                int result = (int)typeof(BlackBoxInt)
+                    .GetField("innerValue", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetValue(boxInstance);
+
+                Console.WriteLine(result);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
